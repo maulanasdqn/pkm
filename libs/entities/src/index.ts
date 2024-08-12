79ceab4d15ef66f8bbbaf5a32cf34afc03226f46
Maulana Sodiqin <53475078+maulanasdqn@users.noreply.github.com>
2024-08-12 13:51:55 +0700

    feat: setup husky (#9)

--- libs/entities/src/index.ts
+++ libs/entities/src/index.ts
@@ -1,3 +1,4 @@
 // Use this file to export React client components (e.g. those with 'use client' directive) or other non-server utilities
 
-export * from './lib/entities';
+export * from './lib/user';
+export * from './lib/token';
--- libs/entities/src/lib/entities.tsx
+++
@@ -1,9 +0,0 @@
-export function Entities() {
-  return (
-    <div>
-      <h1>Welcome to Entities!</h1>
-    </div>
-  );
-}
-
-export default Entities;
