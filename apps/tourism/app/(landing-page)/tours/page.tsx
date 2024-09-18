02b31d952876d887cd0c3f2000329fa0e8603fa9
Muhamad Rhamdhan Mardiansyah <ram.ardiansyah18@gmail.com>
2024-09-18 23:27:45 +0700

    refactpr: fix tours page

--- apps/tourism/app/(landing-page)/tours/page.tsx
+++ apps/tourism/app/(landing-page)/tours/page.tsx
@@ -44,15 +44,15 @@ const tours = [
 ];
 
 const ToursPage: NextPage = async (): Promise<ReactElement> => {
-  const { data } = await getAllDestinations();
-  if (!data) {
-    return (
-      <section className="container mx-auto text-center my-10 space-y-7">
-        <h1>Terjadi kesalahan,data destinasi belum ada/tidak ditemukan!</h1>
-        <Link href="/">Kembali ke beranda</Link>
-      </section>
-    );
-  }
+  // const { data } = await getAllDestinations();
+  // if (!data) {
+  //   return (
+  //     <section className="container mx-auto text-center my-10 space-y-7">
+  //       <h1>Terjadi kesalahan,data destinasi belum ada/tidak ditemukan!</h1>
+  //       <Link href="/">Kembali ke beranda</Link>
+  //     </section>
+  //   );
+  // }
   return <ToursPageModule data={tours} />;
 };
 
