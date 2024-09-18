0a6dcb1cbcbed13f1328ba80159bb0d9a9ad9940
Muhamad Rhamdhan Mardiansyah <ram.ardiansyah18@gmail.com>
2024-09-18 23:15:28 +0700

    refactor : fixing prerender error

--- apps/tourism/app/(landing-page)/_modules/index.tsx
+++ apps/tourism/app/(landing-page)/_modules/index.tsx
@@ -3,14 +3,14 @@ import { DestinationSection, HeroSection, NewsSection } from './sections';
 import { getAllDestinations } from '@pkm/libs/actions/tourism';
 
 export const LandingPageModule: FC = async (): Promise<ReactElement> => {
-  const destinations = await getAllDestinations();
+  // const destinations = await getAllDestinations();
 
   return (
     <>
       <HeroSection />
-      {destinations.data ? (
+      {/* {destinations.data ? (
         <DestinationSection data={destinations.data} />
-      ) : null}
+      ) : null} */}
       <NewsSection />
     </>
   );
