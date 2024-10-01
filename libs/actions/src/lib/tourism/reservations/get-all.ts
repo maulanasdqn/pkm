5b8743dc7dbdd3a0cd5ef11a56f4e75da58108d3
Muhamad Rhamdhan Mardiansyah <ram.ardiansyah18@gmail.com>
2024-10-02 01:06:24 +0700

    refactor : change get data limit

--- libs/actions/src/lib/tourism/reservations/get-all.ts
+++ libs/actions/src/lib/tourism/reservations/get-all.ts
@@ -1,26 +1,15 @@
 'use server';
 
 import { db, destinations, reservations } from '@pkm/libs/drizzle/tourism';
-import {
-  TMetaResponse,
-  TQueryParams,
-  TReservationSchema,
-} from '@pkm/libs/entities';
-import { asc, eq, ilike } from 'drizzle-orm';
+import { TReservationSchema } from '@pkm/libs/entities';
+import { asc, eq } from 'drizzle-orm';
 import { DatabaseError } from 'pg';
 
-export const getAllReservations = async (
-  params?: TQueryParams
-): Promise<{
+export const getAllReservations = async (): Promise<{
   status: { [key: string]: boolean };
   data: TReservationSchema[];
-  meta?: TMetaResponse;
 }> => {
   try {
-    const page = params?.page || 1;
-    const perPage = params?.perPage || 10;
-    const offset = (page - 1) * perPage;
-
     const data = await db
       .select({
         id: reservations.id,
@@ -37,31 +26,12 @@ export const getAllReservations = async (
         },
       })
       .from(reservations)
-      .where(ilike(reservations.name, `%${params?.search || ''}%`))
       .leftJoin(destinations, eq(destinations.id, reservations.destinationId))
-      .limit(perPage)
-      .offset(params?.search ? 0 : offset)
       .orderBy(asc(reservations.date));
-
-    const count = await db
-      .select({ id: reservations.id })
-      .from(reservations)
-      .then((res) => res.length);
-
-    const totalPage = Math.ceil(count / perPage);
-    const nextPage = page < totalPage ? Number(page) + 1 : null;
-    const prevPage = page > 1 ? Number(page - 1) : null;
 
     return {
       status: { ok: true },
       data: data,
-      meta: {
-        page,
-        nextPage,
-        prevPage,
-        perPage,
-        totalPage,
-      },
     };
   } catch (error) {
     if (error instanceof DatabaseError) {
