160dbdd092110aaae2ef6dc3f0198dc8e7833850
EL5225 <luthfiramadhan155@gmail.com>
2024-10-10 14:45:00 +0700

    chore: generate backoffice app #2

--- apps/market/app/(landing)/page.tsx
+++ apps/market/app/(landing)/page.tsx
@@ -1,6 +1,8 @@
 import { NextPage } from 'next';
 import { LandingModule } from './_modules';
 
+export const dynamic = 'auto';
+
 const LandingPage: NextPage = () => {
   return <LandingModule />;
 };
--- apps/market/app/(landing)/products/page.tsx
+++ apps/market/app/(landing)/products/page.tsx
@@ -2,6 +2,8 @@ import { NextPage } from 'next';
 import { ReactElement } from 'react';
 import { ProductsModule } from './_modules';
 
+export const dynamic = 'force-dynamic';
+
 const ProductsPage: NextPage = (): ReactElement => {
   return <ProductsModule />;
 };
